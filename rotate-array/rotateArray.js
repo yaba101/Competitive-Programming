6c728e8e4ef6aba093e0dd40258ed80a734845a4
Yeabsira Mekuria <Yabumek46@gmail.com>
2022-10-17 11:28:26 +0300

    add rotate array solution

---
+++ rotate-array/rotateArray.js
@@ -0,0 +1,15 @@
+const reverse = (nums, start, end) => {
+    while (start < end) {
+        let temp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = temp
+        start++
+        end--
+    }
+}
+const rotateArray = (nums, k) => {
+    k %= nums.length
+    reverse(nums, 0, nums.length - 1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k, nums.length - 1)
+}
